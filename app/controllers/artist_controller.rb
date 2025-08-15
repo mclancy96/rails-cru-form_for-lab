10286ab918483fe38b18d8faf1b6b781bccdfda3
Mike <mclancy96@gmail.com>
2025-08-15 15:52:27 -0400

    Generate controllers and routes for three models

---
+++ app/controllers/artist_controller.rb
@@ -0,0 +1,19 @@
+class ArtistController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
---
+++ app/controllers/genre_controller.rb
@@ -0,0 +1,19 @@
+class GenreController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
